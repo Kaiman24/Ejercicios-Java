6849ce9ca01d67e4f8448c9fe03b7db35165b2ae
Kaiman24 <140463483+Kaiman24@users.noreply.github.com>
2023-08-20 01:01:00 -0600

    Add files via upload

---
+++ SIMPLETRON/JavaApplication6/src/SimpletronLMS/UsaSimpletron.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package SimpletronLMS;
+
+/**
+ *
+ * @author k2g9d
+ */
+public class UsaSimpletron {
+    
+    public static void main(String args[])
+ {  
+    Simpletron miObjeto = new Simpletron();
+    miObjeto.run();
+ }
+    
+}
